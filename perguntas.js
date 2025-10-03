890b23bb5118cb62c3050fa2a8e9ac25c4855fb9
WesleyRz12 <wesley.antunes.machado@escola.pr.gov.br>
2025-10-03 10:11:12 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,19 @@
 criaCartao(
     'Jogos Populares',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Qual é o personagem principal de The Legend of Zelda?',
+    'Link.'
+)
+
+criaCartao(
+    'Curiosidades dos Games',
+    'Em que ano o primeiro jogo de Pac-Man foi lançado?',
+    '1980.'
+)
+
+criaCartao(
+    'Plataformas de Jogo',
+    'Qual serviço de jogos por assinatura da Microsoft oferece acesso a jogos de PC e Xbox?',
+    'Xbox Game Pass.'
 )
 
 criaCartao(
@@ -11,25 +23,31 @@ criaCartao(
 )
 
 criaCartao(
-    'Plataformas de Jogo',
-    'Qual plataforma é famosa por seu serviço de streaming de jogos, o "Stadia"?',
-    'Google.'
-)
-
-criaCartao(
-    'Plataformas de Jogo',
-    'Qual serviço de jogos por assinatura da Microsoft oferece acesso a jogos de PC e Xbox?',
-    'PlayStation.'
-)
-
-criaCartao(
-    'Jogos Populares',
-    'Qual é o objetivo principal do jogo Minecraft?',
-    'Sobreviver, explorar e construir em um mundo gerado proceduralmente.'
+    'Curiosidades dos Games',
+    'Quem é o criador de Super Mario?',
+    'Shigeru Miyamoto.'
 )
 
 criaCartao(
     'Plataformas de Jogo',
     'Qual plataforma de streaming de jogos foi comprada pela Amazon em 2014?',
     'Twitch.'
+)
+
+criaCartao(
+    'Jogos Populares',
+    'O que é Minecraft?',
+    'É um jogo de construção e sobrevivência em um mundo aberto.'
+)
+
+criaCartao(
+    'Plataformas de Jogo',
+    'Qual plataforma é famosa por seu serviço de streaming de jogos, o "Stadia"?',
+    'Google.'
+)
+
+criaCartao(
+    'Curiosidades dos Games',
+    'Qual é o jogo mais vendido de todos os tempos?',
+    'Minecraft.'
 )
